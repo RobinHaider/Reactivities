3c6a6998b7193d9178212a19968a38f5c3b7dc6d
robinhaider <robinhaider69@gmail.com>
2020-11-10 19:29:05 +0600

    4. Building a CRUD application in .Net Core using the CQRS + Mediator pattern

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command: IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
+
+        public class Handler: IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var acitivity = new Activity
+                {
+                    Id = request.Id,
+                    Category = request.Category,
+                    City = request.City,
+                    Date = request.Date,
+                    Description = request.Description,
+                    Title = request.Title,
+                    Venue = request.Venue
+                };
+
+                _context.Activities.Add(acitivity);
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if(success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
